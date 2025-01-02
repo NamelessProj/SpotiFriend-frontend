dba4c92d389d2878ea4d4d94243ce36244a58ae9
Kevin <pintokevin2002@hotmail.com>
2025-01-02 16:15:01 +0100

    Updating the way the error page is displayed

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,10 +3,13 @@ import {Typography} from "@material-tailwind/react";
 const ErrorPage = () => {
     return (
         <main className="h-full flex flex-col justify-center items-center text-primary-white">
-            <Typography variant="h1" className="text-7xl">
+            <Typography variant="h2" as="p">
+                Error
+            </Typography>
+            <Typography variant="h1" className="text-7xl text-primary-green">
                 404
             </Typography>
-            <Typography variant="h2">
+            <Typography variant="h2" as="p">
                 Not Found
             </Typography>
         </main>
