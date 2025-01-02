ecc8978288ecede63ecb31518d83acd463092e82
Kevin <pintokevin2002@hotmail.com>
2025-01-02 16:13:14 +0100

    Updating the storage of the userInfo

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,15 +1,15 @@
 import {create} from "zustand";
 
 export const useAuthStore = create((set) => ({
-    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+    userInfo: localStorage.getItem('spotiUserInfo') ? JSON.parse(localStorage.getItem('spotiUserInfo')) : null,
 
     setCredentials: (data) => {
         set(() => ({userInfo: data}));
-        localStorage.setItem('userInfo', JSON.stringify(data));
+        localStorage.setItem('spotiUserInfo', JSON.stringify(data));
     },
 
     logout: () => {
         set(() => ({userInfo: null}));
-        localStorage.removeItem('userInfo');
+        localStorage.removeItem('spotiUserInfo');
     }
 }));
