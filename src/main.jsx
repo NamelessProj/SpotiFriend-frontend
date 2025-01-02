902589627f525c90dc44036eb488b4056e9a30f8
Kevin <pintokevin2002@hotmail.com>
2025-01-02 18:13:02 +0100

    Importing the configuration of nprogress

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './utils/NProgressConfig.js';
+import App from './App.jsx';
 import {ThemeProvider} from "@material-tailwind/react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
