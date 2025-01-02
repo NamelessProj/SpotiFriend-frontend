850042a70610a41e372fb9cfe318f8056d79fa77
Kevin <pintokevin2002@hotmail.com>
2025-01-02 16:13:32 +0100

    Adding a button to login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
-import {Typography} from "@material-tailwind/react";
+import {Button, Typography} from "@material-tailwind/react";
+import {useAuthStore} from "../stores/authStore.js";
+import {Link} from "react-router-dom";
 
 const Header = () => {
+    const {userInfo} = useAuthStore();
+
     return (
-        <header className="flex justify-center items-center">
+        <header className="flex flex-col-reverse gap-2 justify-center items-center my-2 relative">
             <Typography variant="h2" as="p" className="text-primary-green">
                 SpotiFriend
             </Typography>
+
+            <div className="md:absolute top-1/2 right-2 md:transform md:-translate-y-1/2">
+                <Button size="sm" color="green" className="text-primary-black">
+                    {userInfo ? (
+                        <Link to="profile">
+                            {userInfo.username}
+                        </Link>
+                    ):(
+                        <Link to="login">
+                            Login
+                        </Link>
+                    )}
+                </Button>
+            </div>
         </header>
     );
 };
