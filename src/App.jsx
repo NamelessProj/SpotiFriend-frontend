9ae1121e2de987735e66ce3cc631a9cde5d532d3
Kevin <pintokevin2002@hotmail.com>
2025-01-02 18:09:29 +0100

    Adding the route to access the login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 import Room from "./pages/Room.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout /> }>
                 <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
                 <Route path="room/:id" element={<Room />} />
                 <Route path="*" element={<ErrorPage />} />
             </Route>
