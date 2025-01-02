9efdf6dd645acc662f8cbf1b9e3239367e7199f9
Kevin <pintokevin2002@hotmail.com>
2025-01-02 18:08:55 +0100

    Adding the form for the register

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,113 @@
+import {useEffect, useState} from "react";
+import {useUserStore} from "../stores/userStore.js";
+import {useAuthStore} from "../stores/authStore.js";
+import {Alert, Button, Card, CardBody, CardHeader, Input, Typography} from "@material-tailwind/react";
+
+const RegisterForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [error, setError] = useState("");
+
+    const {user, userError, register} = useUserStore();
+    const {setCredentials} = useAuthStore();
+
+    useEffect(() => {
+        if(user){
+            setCredentials(user);
+        }
+    }, [user]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+        const emailForm = email.trim();
+        const passwordForm = password.trim();
+        const confirmPasswordForm = confirmPassword.trim();
+        const usernameForm = username.trim();
+
+        if(emailForm === "" || passwordForm === "" || confirmPasswordForm === "" || usernameForm === ""){
+            setError("Please fill in all the fields.");
+            return;
+        }
+
+        if(!emailRegex.test(emailForm)){
+            setError("Please enter a valid email.");
+            return;
+        }
+
+        if(passwordForm !== confirmPasswordForm){
+            setError("Passwords do not match.");
+            return;
+        }
+
+        try{
+            await register({email: emailForm, password: passwordForm, username: usernameForm});
+        }catch(error){
+            setError(error);
+        }
+    }
+
+    return (
+        <div className="mt-6 flex justify-center">
+            <Card className="w-96">
+                <CardHeader variant="gradient" color="green" className="mb-4 grid h-28 place-items-center">
+                    <Typography variant="h3">
+                        Register
+                    </Typography>
+                </CardHeader>
+                <CardBody>
+                    {error && (
+                        <Alert color="red" className="mb-6">
+                            {error}
+                        </Alert>
+                    )}
+                    {userError && (
+                        <Alert color="blue" className="mb-6">
+                            {userError}
+                        </Alert>
+                    )}
+                    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+                        <Input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            inputMode="email"
+                            size="lg"
+                            label="Email"
+                        />
+                        <Input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            size="lg"
+                            label="Username"
+                        />
+                        <Input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            size="lg"
+                            type="password"
+                            label="Password"
+                        />
+                        <Input
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            size="lg"
+                            type="password"
+                            label="Confirm Password"
+                        />
+                        <Button color="green" variant="gradient" onClick={handleSubmit}>
+                            Register
+                        </Button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default RegisterForm;
