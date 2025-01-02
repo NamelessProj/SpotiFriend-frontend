6cd365e26383dd60bd76a55b2ebf23097a883c4e
Kevin <pintokevin2002@hotmail.com>
2025-01-02 20:45:47 +0100

    Adding the route to access the user profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
 import Room from "./pages/Room.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Layout /> }>
                 <Route index element={<Home />} />
                 <Route path="login" element={<Login />} />
+                <Route path="profile" element={<UserProfile />} />
                 <Route path="room/:id" element={<Room />} />
                 <Route path="*" element={<ErrorPage />} />
             </Route>
