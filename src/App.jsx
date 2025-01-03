c01e62ae0f26ec865a5fe49b207de10c1c14e88b
Kevin <pintokevin2002@hotmail.com>
2025-01-03 14:19:31 +0100

    Adding the route to the admin home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
+import AdminLayout from "./components/AdminLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import UserProfile from "./pages/UserProfile.jsx";
 import Room from "./pages/Room.jsx";
+import AdminHome from "./pages/AdminHome.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Layout /> }>
+            <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="login" element={<Login />} />
                 <Route path="profile" element={<UserProfile />} />
                 <Route path="room/:id" element={<Room />} />
+                <Route path="admin" element={<AdminLayout />}>
+                    <Route index element={<AdminHome />} />
+                </Route>
                 <Route path="*" element={<ErrorPage />} />
             </Route>
         </Routes>
