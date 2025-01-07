05f821412649a0d63767bac4f8de0213ba91e0d8
Pinto <pintokevin2002@hotmail.com>
2025-01-07 12:50:05 +0100

    Updating the Admin page

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,23 +1,8 @@
-import {useTokenStore} from "../stores/tokenStore.js";
-import {useEffect} from "react";
-
 const AdminHome = () => {
-    const {token, getToken} = useTokenStore();
-
-    useEffect(() => {
-        if(token){
-            const createdAt = new Date(token.createdAt);
-            if((new Date() - createdAt) > 1000 * 60 * 60) getToken();
-        }else getToken();
-    }, []);
 
     return (
         <div>
-            {token && (
-                <div className="text-primary-white">
-                    {token.token}
-                </div>
-            )}
+            <h1>Admin Home</h1>
         </div>
     );
 };
