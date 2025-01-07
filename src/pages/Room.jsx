b806e93fceb456861ec3f52c9023c9cea99fd6d1
Pinto <pintokevin2002@hotmail.com>
2025-01-07 12:43:23 +0100

    Getting the room data

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,45 @@
 import {useParams} from "react-router-dom";
+import {useEffect} from "react";
+import {useRoomStore} from "../stores/roomStore.js";
+import {Alert, Typography} from "@material-tailwind/react";
 
 const Room = () => {
     const {id} = useParams();
 
+    const {room, roomLoading, roomError, getRoomById} = useRoomStore();
+
+    useEffect(() => {
+        getRoomById(id);
+    }, []);
+
     return (
-        <div className="text-primary-white">
-            {id}
-        </div>
+        <main>
+            {roomError && (
+                <Alert color="red" className="mb-6">
+                    {roomError}
+                </Alert>
+            )}
+            {roomLoading ? (
+                <p>Loading...</p>
+            ):(
+                <>
+                    {room ? (
+                        <div>
+                            <div>
+                                <Typography variant="h2" className="text-primary-white">
+                                    {room.name}
+                                </Typography>
+                                <Typography className="text-primary-white">
+                                    {room.description}
+                                </Typography>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>No room</p>
+                    )}
+                </>
+            )}
+        </main>
     );
 };
 
