a84a8d4ed1672f8a1bafa347631bf2b19f6ecfc2
Kevin <pintokevin2002@hotmail.com>
2025-01-08 16:51:51 +0100

    Adding the ToastContainer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
+import {ToastContainer} from "react-toastify";
 
 const Layout = () => {
     return (
         <div className="grid grid-rows-app min-h-[100svh] bg-primary-black">
+            <ToastContainer theme="dark" />
             <Header />
             <Outlet />
             <Footer />
