6cc5c436b600b2ea4caaa31321732e5bda2b8945
Kevin <pintokevin2002@hotmail.com>
2025-01-08 17:06:30 +0100

    Adding propositionSuccess

--- src/stores/propositionStore.js
+++ src/stores/propositionStore.js
@@ -4,12 +4,13 @@ import axios from "axios";
 export const usePropositionStore = create((set) => ({
     propositionError: null,
     propositionLoading: false,
+    propositionSuccess: false,
 
     sendProposition: async (id, data) => {
-        set({propositionLoading: true, propositionError: null});
+        set({propositionLoading: true, propositionError: null, propositionSuccess: false});
         try{
             await axios.post(`${import.meta.env.VITE_API_URL}/proposition/${id}`, data);
-            set(() => ({propositionLoading: false}));
+            set(() => ({propositionLoading: false, propositionSuccess: true}));
         }catch(error){
             set({propositionError: error.response.data.message || error.message, propositionLoading: false});
         }
