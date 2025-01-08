9534a2c6370d968fe0f2f981b42f6681c8f92397
Kevin <pintokevin2002@hotmail.com>
2025-01-08 17:38:29 +0100

    Updating the display of the TrackCard

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -3,8 +3,10 @@ import {Button, Card, CardBody, CardFooter, Typography} from "@material-tailwind
 const TrackCard = ({track, handleSendProposition}) => {
     return (
         <Card>
-            <CardBody>
-                <img src={track.album.images[0].url} alt={track.name} loading="lazy" className="w-full h-48 object-cover mb-4 rounded-lg"/>
+            <CardBody className="flex flex-col items-center md:grid grid-rows-2 grid-cols-[120px_1fr] md:gap-x-3">
+                <div className="h-32 md:h-12 mb-4 md:row-span-2">
+                    <img src={track.album.images[0].url} alt={track.name} loading="lazy" className="h-32 md:h-auto object-cover mb-4 rounded-lg"/>
+                </div>
                 <Typography variant="h3">
                     {track.name}
                 </Typography>
@@ -12,9 +14,9 @@ const TrackCard = ({track, handleSendProposition}) => {
                     {track.artists.map((artist) => artist.name).join(", ")}
                 </Typography>
             </CardBody>
-            <CardFooter>
-                <Button color="green" variant="gradient" onClick={(e) => handleSendProposition(e, track)}>
-                    Send d Proposition
+            <CardFooter className="flex md:mt-3">
+                <Button color="green" variant="gradient" className="flex-grow" onClick={(e) => handleSendProposition(e, track)}>
+                    Send Proposition
                 </Button>
             </CardFooter>
         </Card>
