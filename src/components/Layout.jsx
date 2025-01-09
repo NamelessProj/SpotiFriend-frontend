0041fc661c396d2dfc751be0f1dae58369589c59
Kevin <pintokevin2002@hotmail.com>
2025-01-09 15:48:11 +0100

    Using the SpeedInsights component from the @vercel/speed-insights library

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
 import {ToastContainer} from "react-toastify";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 const Layout = () => {
     return (
@@ -10,6 +11,7 @@ const Layout = () => {
             <Header />
             <Outlet />
             <Footer />
+            <SpeedInsights />
         </div>
     );
 };
