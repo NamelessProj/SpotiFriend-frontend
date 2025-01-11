2eab2504cc821aad316c38c7af13ac970a89e30f
Kevin <pintokevin2002@hotmail.com>
2025-01-11 12:54:08 +0100

    Displaying a spinner when handling the process of login or registering

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,14 @@ import {Tab, TabPanel, Tabs, TabsBody, TabsHeader} from "@material-tailwind/reac
 import LoginForm from "../components/LoginForm.jsx";
 import RegisterForm from "../components/RegisterForm.jsx";
 import {useAuthStore} from "../stores/authStore.js";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import DefaultSpinner from "../components/DefaultSpinner.jsx";
 
 const Login = () => {
     const {userInfo} = useAuthStore();
+
+    const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
 
@@ -16,32 +19,38 @@ const Login = () => {
 
     return (
         <main>
-            <div className="max-w-[500px] mx-auto mt-6">
-                <Tabs value="login">
-                    <TabsHeader className="mb-3">
-                        <Tab value="login">
-                            Login
-                        </Tab>
-                        <Tab value="register">
-                            Register
-                        </Tab>
-                    </TabsHeader>
-                    <TabsBody
-                        animate={{
-                            initial: { x: 250 },
-                            mount: { x: 0 },
-                            unmount: { x: 250 },
-                        }}
-                    >
-                        <TabPanel value="login">
-                            <LoginForm />
-                        </TabPanel>
-                        <TabPanel value="register">
-                            <RegisterForm />
-                        </TabPanel>
-                    </TabsBody>
-                </Tabs>
-            </div>
+            {loading ? (
+                <div className="h-full flex justify-center items-center">
+                    <DefaultSpinner />
+                </div>
+            ):(
+                <div className="max-w-[500px] mx-auto mt-6">
+                    <Tabs value="login">
+                        <TabsHeader className="mb-3">
+                            <Tab value="login">
+                                Login
+                            </Tab>
+                            <Tab value="register">
+                                Register
+                            </Tab>
+                        </TabsHeader>
+                        <TabsBody
+                            animate={{
+                                initial: {x: 250},
+                                mount: {x: 0},
+                                unmount: {x: 250},
+                            }}
+                        >
+                            <TabPanel value="login">
+                                <LoginForm setLoading={setLoading}/>
+                            </TabPanel>
+                            <TabPanel value="register">
+                                <RegisterForm setLoading={setLoading}/>
+                            </TabPanel>
+                        </TabsBody>
+                    </Tabs>
+                </div>
+            )}
         </main>
     );
 };
